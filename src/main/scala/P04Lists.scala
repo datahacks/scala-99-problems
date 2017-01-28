5c4ec5ad1a606143a801d6e983a138cabfc434e7
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-01-28 10:06:19 -0500

    Solution for P05

--- src/main/scala/P04Lists.scala
+++ src/main/scala/P04Lists.scala
@@ -6,13 +6,6 @@
  * res0: Int = 6
  */
 
-/**
- * P04 (*) Find the number of elements of a list.
- * Example:
- * scala> length(List(1, 1, 2, 3, 5, 8))
- * res0: Int = 6
- */
-
 object P04 {
   
   def main(args: Array[String]): Unit = {
