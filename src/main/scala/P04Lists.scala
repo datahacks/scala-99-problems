e0c6aaff75544dea4335b85c45fb983f8abcc58a
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-01-28 10:04:36 -0500

    Solution for P05

--- src/main/scala/P04Lists.scala
+++ src/main/scala/P04Lists.scala
@@ -6,6 +6,12 @@
  * res0: Int = 6
  */
 
+/**
+ * P04 (*) Find the number of elements of a list.
+ * Example:
+ * scala> length(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 6
+ */
 
 object P04 {
   
---
+++ src/main/scala/P05Lists.scala
@@ -0,0 +1,26 @@
+
+/**
+ * (*) Reverse a list.
+ * Example:
+ * scala> reverse(List(1, 1, 2, 3, 5, 8))
+ * res0: List[Int] = List(8, 5, 3, 2, 1, 1)
+ */
+
+object P05 {
+  
+  def main(args: Array[String]): Unit = {
+    val xs = List("abc", "def", "ghi")
+    xs.foreach(println)
+    println("====reverse====")
+    reverseTailRecursive(xs).foreach(println)
+  }
+  
+  def reverseTailRecursive[T](list: List[T]) = {
+    def accum[T](result:List[T], xs: List[T]) : List[T] =  xs match  {
+      case Nil => result
+      case h :: tail => accum(h :: result, tail)
+   
+    }
+    accum(List[T](), list)
+  }
+}
