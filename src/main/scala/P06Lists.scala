c17317cdf5eb311023fe27d00d727177991230ef
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-01-31 06:28:03 -0500

    solution for P06

---
+++ src/main/scala/P06Lists.scala
@@ -0,0 +1,20 @@
+
+/**
+ * P06 (*) Find out whether a list is a palindrome.
+ * Example:
+ * scala> isPalindrome(List(1, 2, 3, 2, 1))
+ * res0: Boolean = true
+ */
+
+object P06 {
+  
+  def main(args: Array[String]): Unit = {
+    val xs = List(1,2,3,2,1)
+    val ys = List(1,2,3,1,1)
+    println(isPalindrome(xs))
+    println(isPalindrome(ys))
+  }
+  
+  def isPalindrome[T](list: List[T]): Boolean =
+    list == list.reverse
+}
