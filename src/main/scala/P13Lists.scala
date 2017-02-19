ca5ee18c2eac5733ce86d27997e22cf921c97497
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-02-18 20:20:04 -0500

    solution for P13

--- src/main/scala/P13Lists.scala
+++ src/main/scala/P13Lists.scala
@@ -11,4 +11,22 @@
 
 object P13Lists {
   
+  def main(args: Array[String]): Unit = {
+    val ls = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    println(encodeDirect(ls))
+  }
+  
+  def encodeDirect(list: List[Symbol]): List[(Int, Symbol)] = {
+    
+    def accum(result: List[(Int,Symbol)], prev: Symbol, xs: List[Symbol], n: Int ): 
+    List[(Int, Symbol)] = xs match {
+      
+      case Nil => result.reverse
+      case h:: tail if h == prev => accum((n+1,h) :: result.tail, h, tail, n+1  )
+      case h:: tail => accum((1,h):: result,h,tail, 1)
+      
+    }
+    accum(List((1, list.head)), list.head, list.tail, 1)
+  }
+  
 }
