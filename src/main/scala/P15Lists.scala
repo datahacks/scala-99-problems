b48e5c4f59f7fcf934e294189a578ba6f4a2b226
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-02-21 07:07:01 -0500

    solution for P15

--- src/main/scala/P15Lists.scala
+++ src/main/scala/P15Lists.scala
@@ -8,6 +8,20 @@
 
 object P15Lists {
   
-  def duplicateN = ???
+  def main(args: Array[String]): Unit = {
+    val ls = List('a, 'b, 'c, 'c, 'd)
+    println(duplicateN(3, ls))
+  }
+  
+  def duplicateN(n: Int, list: List[Symbol] ) = {
+    
+    def accum(i: Int, sym: Symbol, result: List[Symbol]): List[Symbol] = i match {
+      case i if i == 0 => result
+      case i  => accum(i - 1, sym, sym:: result) 
+    }
+    
+    list.flatMap { x => accum(n,x, List()) }
+    
+  }
   
 }
