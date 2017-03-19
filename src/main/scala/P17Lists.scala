1adbf5cf9ed63d28dd55949ffdca5f3a397ce6e7
Prashant Raghuvanshi <datahacks22@gmail.com>
2017-03-19 15:19:49 -0400

    P17 recursive solution

--- src/main/scala/P17Lists.scala
+++ src/main/scala/P17Lists.scala
@@ -15,10 +15,22 @@ object P17Lists {
   def main(args: Array[String]): Unit = {
     val ls = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
     println(splitBuiltIn(3,ls))
+    
+    println(splitRecursive(3,ls))
   }
   
   // using builtin splitAt
   def splitBuiltIn(n: Int, list: List[Symbol]) = list.splitAt(n)
   
-  //TODO try recursive option instead of using splitAt
+  // try recursive option instead of using splitAt
+  def splitRecursive(n: Int, list: List[Symbol]) = {
+    
+    def accum(result1: List[Symbol], 
+        result2: List[Symbol], 
+        i: Int, xs: List[Symbol]): (List[Symbol], List[Symbol]) = xs match {
+      case h:: tail if (i == 0) => (result1.reverse, h:: tail ::: result2)
+      case h:: tail => accum(h:: result1, result2, i-1, tail) 
+    }
+    accum(List(), List(), n, list)
+  }
 }
